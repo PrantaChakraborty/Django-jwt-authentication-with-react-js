51bc0c1b66c05b6d77f7614bdcc17557c6a44a5d
PrantaChakraborty <cbpranta@gmail.com>
2021-01-26 20:06:15 +0600

    added CustomUser model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+# custom user model
+
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(blank=False, null=False,  max_length=3)
