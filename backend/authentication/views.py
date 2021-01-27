5b8c255de306380e0c861dfebc47e8a4c43c5cf4
PrantaChakraborty <cbpranta@gmail.com>
2021-01-27 11:06:13 +0600

    views form creating custom user

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,10 +1,25 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import permissions
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .serializers import MyTokenObtainPairSerializer
+from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
 
 
 # view 'age' field with user details in token
 class ObtainTokenPairWithAgeView(TokenObtainPairView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+
+# custom user create
+class CustomUserCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
