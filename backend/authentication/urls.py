19d6b4af05a5d86f949b86f17e2e0a578ad8776f
PrantaChakraborty <cbpranta@gmail.com>
2021-01-27 10:48:26 +0600

    write views for display token with extra customUser field

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt import views
+from .views import ObtainTokenPairWithAgeView
 
 
 urlpatterns = [
-    path('token/obtain/', views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/obtain/', ObtainTokenPairWithAgeView.as_view(), name='token_create'),
     path('token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
