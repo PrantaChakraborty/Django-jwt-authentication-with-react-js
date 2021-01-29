c7ae8e8413b9db7a0d0229b0141647339ba7b2d8
PrantaChakraborty <cbpranta@gmail.com>
2021-01-29 11:57:57 +0600

    added navbar in react

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Login</a>
+                </li>
+                <li>
+                    <a href="#">Signup</a>
+                </li>
+                <li>
+                    <a href="#">Logout</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
