9cc1eb7579b2eb4b5f2e425d1def7fdc43703e2f
PrantaChakraborty <cbpranta@gmail.com>
2021-01-29 12:03:35 +0600

    installed CORS headers

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views
-from .views import ObtainTokenPairWithAgeView, CustomUserCreate
+from .views import ObtainTokenPairWithAgeView, CustomUserCreate, HelloWorld
 
 
 urlpatterns = [
     path('user/create/', CustomUserCreate.as_view(), name='create_user'),
     path('token/obtain/', ObtainTokenPairWithAgeView.as_view(), name='token_create'),
     path('token/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', HelloWorld.as_view(), name='hello_world'),
 ]
