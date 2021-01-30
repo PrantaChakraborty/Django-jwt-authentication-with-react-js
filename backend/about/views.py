bc564a61a85e0dcadd84c338e023a1d89bb09e25
PrantaChakraborty <cbpranta@gmail.com>
2021-01-30 12:24:12 +0600

    view for about

--- backend/about/views.py
+++ backend/about/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework import permissions
+from .models import About
+from .serializers import AboutSerializers
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes(permissions.AllowAny)
+def about_view(request):
+    about = About.objects.all()
+    serializer = AboutSerializers(about, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
