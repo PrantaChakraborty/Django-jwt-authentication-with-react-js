5761f98ef844b6abb47d9aa417dbf2eb6b97abc3
PrantaChakraborty <cbpranta@gmail.com>
2021-01-30 12:14:20 +0600

    added About serializers

---
+++ backend/about/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import About
+
+
+class AboutSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = About
+        fields= ('name', 'address', 'phone')
